374f59a9f68f74272743636576df4624206b7436
Shamov-Aleksandr-RIS-21-1b <shamov.03@list.ru>
2023-01-02 18:48:39 +0500

    add database connection

---
+++ connect_database.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', "127.0.0.1");
+define('DB_USERNAME', "root");
+define('DB_PASSWORD', "");
+define('DB_NAME', "Gallows");
+$db_induction = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+?>
